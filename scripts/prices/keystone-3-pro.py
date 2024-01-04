b6650a73d02353044fbf66c958d92dfd70e7c14e
Maxi Rosson <maxirosson@gmail.com>
2024-01-03 23:12:47 -0300

    Keystone 3 price fix

--- scripts/prices/keystone-3-pro.py
+++ scripts/prices/keystone-3-pro.py
@@ -13,6 +13,6 @@ for product in data['result']:
         price = product['discountPrice']
         break
 
-assert price == 129.0, f"Failed: Price '{price}' does not match expected value"
+assert price == 100.62, f"Failed: Price '{price}' does not match expected value"
 
 print(price)
